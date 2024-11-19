c20d253d4396805c4c3e1ef62a4705ac4297f244
meupaiomeupai <111287354+meupaiomeupai@users.noreply.github.com>
2024-11-19 16:01:52 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,20 +1,20 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function vizualizarInformacoesGlobais() {
+async function visualizarInformacoesTintasGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasUsandoTintas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+    const porcentagemUsandoTintas = ((pessoasUsandoTintas / pessoasNoMundo) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    paragrafo.classList.add('graficos-container__texto-tinta') // Classe ajustada para o tema de tinta
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasUsandoTintas} bilhões</span> utilizam algum tipo de produto de tinta, passando em média <span>${horas} horas</span> e <span>${minutos} minutos</span> por ano em atividades de pintura e manutenção de superfícies.<br>Isso significa que aproximadamente <span>${porcentagemUsandoTintas}%</span> da população utiliza tintas em suas residências ou locais de trabalho.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
 
-vizualizarInformacoesGlobais()
+visualizarInformacoesTintasGlobais()
