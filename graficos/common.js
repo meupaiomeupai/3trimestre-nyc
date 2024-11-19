53974560486065bc2448cce655229d4b5fe3dbe6
meupaiomeupai <111287354+meupaiomeupai@users.noreply.github.com>
2024-11-19 15:59:24 -0300

    Update common.js

--- graficos/common.js
+++ graficos/common.js
@@ -3,14 +3,14 @@ const getCSS = (variavel) => {
 }
 
 const tickConfig = {
-    color: getCSS('--primary-color'),
+    color: getCSS('--primary-color'), // cor primária para o tema de tinta
     size: 16,
     family: getCSS('--font')
 }
 
 function criarGrafico(data, layout) {
     const grafico = document.createElement('div')
-    grafico.className = 'grafico'
+    grafico.className = 'grafico-tinta' // nome de classe ajustado para tema de tinta
     document.getElementById('graficos-container').appendChild(grafico)
     const config = {
         responsive: true,
@@ -22,7 +22,7 @@ function criarGrafico(data, layout) {
 function incluirTexto(texto) {
     const container = document.getElementById('graficos-container')
     const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.classList.add('graficos-container__texto-tinta') // nome de classe ajustado para tema de tinta
     paragrafo.innerHTML = texto
     container.appendChild(paragrafo)
 }
